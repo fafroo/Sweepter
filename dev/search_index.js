b3be677c00adac3db0d366e8241fe8d431537bf5
Documenter.jl <documenter@juliadocs.github.io>
2022-11-07 16:02:21 +0000

    build based on b94fa83

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sweepter","category":"page"},{"location":"#Sweepter","page":"Home","title":"Sweepter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sweepter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sweepter]","category":"page"}]
+}
